af14ceb9aa6f8fbd19f2a80d62f22c23bc5bc970
the-groot <sangwoo0795@naver.com>
2021-07-08 15:49:36 +0900

    #7강의 업로드

--- momentum/js/greeting.js
+++ momentum/js/greeting.js
@@ -6,12 +6,13 @@ const HIDDEN_CLASSNAME="hidden";
 const USERNAME_KEY="username"
 
 function onLoginSubmit(event){
-    console.dir(LoginInput);
-    const username=LoginInput.value;
-    event.preventDefault();
-    localStorage.setItem(USERNAME_KEY,username);
-    LoginForm.classList.add(HIDDEN_CLASSNAME);
-    paintGreetings(username);
+
+
+    const username=LoginInput.value;        //username에 LoginInput text 저장
+    event.preventDefault();                 //새로고침 막음
+    localStorage.setItem(USERNAME_KEY,username);    //localStorage에 key : "username" , value : "text" 저장
+    LoginForm.classList.add(HIDDEN_CLASSNAME);      //LoginForm hidden해줌
+    paintGreetings(username);                       //paintGreetings()호출
 }
 
 function handleLinkClick(event){
@@ -20,19 +21,19 @@ function handleLinkClick(event){
 }
 
 function paintGreetings(username){
-    greeting.innerText=`Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText=`Hello ${username}`;     //html의 greeting부분의 text를 Hello text형식으로 화면에 출력
+    greeting.classList.remove(HIDDEN_CLASSNAME);    //greeting hidden해줌
 }
 
 
-const savedUsername=localStorage.getItem(USERNAME_KEY);
+const savedUsername=localStorage.getItem(USERNAME_KEY); //localStorage에 저장된 username값 받아옴
 
-if(savedUsername===null){
-    console.log("what?");
-    LoginForm.classList.remove(HIDDEN_CLASSNAME);
-    LoginForm.addEventListener("submit",onLoginSubmit);
+if(savedUsername===null){       //localStorage에 저장된 username이 비어있으면
+    LoginForm.classList.remove(HIDDEN_CLASSNAME);   //hidden 지우고
+    LoginForm.addEventListener("submit",onLoginSubmit);     //submit이벤트 기다림
+                                                            //submit되면 onLoginSubmit()호출
 }
 
-else{
-    paintGreetings(savedUsername);
+else{       //localStorage에 저장된 username이 없으면
+    paintGreetings(savedUsername);  //paintGreetings() 호출
 }
--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -7,46 +7,63 @@ const TODOS_KEY="todos";
 let toDos=[];
 
 
-function saveToDos(){
-    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+function saveToDos(){   
+    //데이터 localStorage에 저장하는 함수
+
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //key : todos , value : {text, id} stringify는 string형태로 저장하는 것
 }
 
 function deleteTodo(event){
-    const li=event.target.parentElement;
-    li.remove();
+    //데이터 삭제하는 함수
+
+    const li=event.target.parentElement;        //event.target.parentElement가 눌려진 버튼의 정보를 담고있음
+    li.remove();                                //제거하고
+    toDos=toDos.filter((toDo) => toDo.id !==parseInt(li.id));   //li.id와 다른것들은 toDos에 그대로 저장(li.id와 toDo.id가 같으면 배열에서 제거)
+    saveToDos();        //saveToDos()호출
 }
 
 function paintToDo(newTodo){
-    const li=document.createElement("li");
-    const span=document.createElement("span");
-    span.innerText=newTodo;
-    const button=document.createElement("button");
-    button.innerText="❌";
-    button.addEventListener("click",deleteTodo);
+    //화면에 데이터를 출력하는 함수
+
+    const li=document.createElement("li");      //li를 html에 만들어줌
+    li.id=newTodo.id;                           //li.id에 newTodo.id 저장
+    const span=document.createElement("span");  //span을 html에 만들어줌
+    span.innerText=newTodo.text;                //span에 newTodo.text를 출력
+    const button=document.createElement("button");  //button을 만들어줌
+    button.innerText="❌";                          //button을 x로 화면에 출력
+    button.addEventListener("click",deleteTodo);    //x버튼이 눌렸을때 deleteTodo()호출
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event){
-    event.preventDefault();
-    const newTodo=toDoInput.value;
-    toDoInput.value="";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
-    saveToDos();
+    //submit이 일어났을때 값을 toDos에 저장하는 함수
+
+    event.preventDefault();     //submit했을때 새로고침 막아주고
+    const newTodo=toDoInput.value;  //input값(text)를 newTodo 변수에 저장
+    toDoInput.value="";     //그리고 toDoInput.value를 비워줌
+    const newTodoObj={  //객체를 만들어 text와 id설정
+        text: newTodo,
+        id : Date.now(),
+    };
+    toDos.push(newTodoObj); //toDos에 객체 값 저장
+    paintToDo(newTodoObj);  //paintToDo()호출
+    saveToDos();            //saveToDos()호출
 }
 
-toDoForm.addEventListener("submit",handleToDoSubmit);
+toDoForm.addEventListener("submit",handleToDoSubmit);       //클릭이나 엔터(submit)하면 handleToDoSubmit호출
 
 function sayHello(){
     console.log("hello");
 }
 
-const savedToDos=localStorage.getItem(TODOS_KEY);  
+const savedToDos=localStorage.getItem(TODOS_KEY);   //localStorage에 저장된 값을 불러옴 
 
-if(savedToDos){
-    const parsedToDos=JSON.parse(savedToDos);
-    toDos=parsedToDos;
-    parsedToDos.forEach(paintToDo);
+if(savedToDos){     //savedToDos가 비어있지 않으면
+    const parsedToDos=JSON.parse(savedToDos);   //savedToDos는 saveToDos()로 인해 string형태로 저장돼있으므로
+                                                //string을 array형태로 다시 저장해줌 
+    toDos=parsedToDos;                          //그걸 다시 toDos에 저장해줌
+    parsedToDos.forEach(paintToDo);             //각각 paintToDo호출
 }
+ 
